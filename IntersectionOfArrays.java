2520c9b9a87c2ee4eef09bfb82c42391007bd2d0
aalexcomas11 <comasrent@yahoo.com>
2019-06-01 23:43:10 -0400

    solved Intersetion of Arrays

---
+++ IntersectionOfArrays.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer,Integer> table = new HashMap<>();
+        
+        for(int num : nums1){
+            if(!table.containsKey(num)){
+                table.put(num,1);
+            }
+            else{
+                table.put(num, table.get(num)+1);
+            }
+        }
+        
+        int[] ans = new int[Math.min(nums1.length,nums2.length)];
+        int i = 0;
+        
+        for(int num: nums2){
+            
+            if(table.containsKey(num) && table.get(num) > 0){
+                ans[i++] = num;
+                table.put(num, table.get(num)-1);
+            }
+        }
+        
+        return Arrays.copyOf(ans,i);
+        
+        
+    }
+}
